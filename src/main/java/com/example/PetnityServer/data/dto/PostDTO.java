b6b4b3f5d6635c31a05e7de1dfc3102b9e3092b3
JaeJunLee <lecile333@gmail.com>
2024-07-27 20:42:20 +0900

    create postDTO.java
    postDTO.java 파일을 새로 작성 했습니다. 확장성을 생각해 inner class 형식으로 작성했습니다.

---
+++ src/main/java/com/example/PetnityServer/data/dto/PostDTO.java
@@ -0,0 +1,54 @@
+package com.example.PetnityServer.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class PostDTO {
+    public static class Request {
+        @Getter
+        @AllArgsConstructor
+        @Builder
+        public static class Create {
+            private String password;
+            private String title;
+            private String message;
+            private Short dogAge;
+            private String dogName;
+            private String location;
+        }
+
+        @Getter
+        @AllArgsConstructor
+        @Builder
+        public static class Update {
+            //private String password;
+            private String title;
+            private String message;
+            private Short dogAge;
+            private String dogName;
+            private String location;
+        }
+        public static class Delete {
+            @Getter
+            @AllArgsConstructor
+            @Builder
+            public static class ById {
+                private Long id;
+            }
+        }
+    }
+    public static class Response {
+        @Getter
+        @AllArgsConstructor
+        @Builder
+        public static class Read {
+            private Long id;
+            private String title;
+            private String message;
+            private Short dogAge;
+            private String dogName;
+            private String location;
+        }
+    }
+}
